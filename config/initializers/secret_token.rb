3a7b92fea817d1673a1e52af2eedb13d46557486
Dan Schneiderman <schneiderman.daniel@gmail.com>
2014-01-29 20:53:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarkdownEditor::Application.config.secret_key_base = '5110dd2e1e60cc2b732cb70aec7487c23b4860dc9a0169539658ee7c9433bbe445dc239deb9eebc14d1caa7726d6634c0591ea3a8cfaa98cf722e74065ba22de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MarkdownEditor::Application.config.session_store :cookie_store, key: '_MarkdownEditor_session'
