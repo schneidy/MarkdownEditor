4129a69d5df72128c287255534ed5d0f41167bfa
Dan Schneiderman <schneiderman.daniel@gmail.com>
2014-02-05 22:32:21 -0500

    Fixed respond_to errors

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -15,10 +15,14 @@ class PapersController < ApplicationController
     if params[:submit] == 'Save'
       current_user.papers << @paper
       if @paper.save
-        format.html { redirect_to edit_paper_path(@paper), notice: 'Paper was successfully created.' }
+        respond_to do |format|
+          format.html { redirect_to edit_paper_path(@paper), notice: 'Paper was successfully created.' }
+        end
       else
-        format.html { render action: 'new' }
-        format.json { render json: @paper.errors, status: :unprocessable_entity }
+        respond_to do |format|
+          format.html { render action: 'new' }
+          format.json { render json: @paper.errors, status: :unprocessable_entity }
+        end
       end
     # Else will equal Render  
     else
@@ -36,15 +40,21 @@ class PapersController < ApplicationController
       if @paper.update(paper_params)
         redirect_to root_path, notice: 'Paper was successfully created.'
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @paper.errors, status: :unprocessable_entity }
+        respond_to do |format|
+          format.html { render action: 'edit' }
+          format.json { render json: @paper.errors, status: :unprocessable_entity }
+        end
       end
     # Else will equal Render  
     else
       if @paper.update(paper_params)
         redirect_to edit_paper_path(@paper)
       else
-        format.json { render json: @paper.errors, status: :unprocessable_entity }
+        respond_to do |format|
+          format.html { render action: 'edit' }
+          format.json { render json: @paper.errors, status: :unprocessable_entity }
+        
+        end
       end
     end
   end
